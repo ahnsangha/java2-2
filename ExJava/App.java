cf848c396718cf223baf8dcfb73d50f4580c0250
ahnsangha <202130311@email.daelim.ac.kr>
2024-04-19 15:39:22 +0900

    4월19일

--- ExJava/App.java
+++ ExJava/App.java
@@ -1,5 +1,8 @@
+import java2.Cal;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        Cal ahn = new Cal();
+        System.out.println(ahn.add(12, 34));
     }
 }
