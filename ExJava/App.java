7a71091c9b995a4871d8ade99a8f7a63482b8af7
ahnsangha <202130311@email.daelim.ac.kr>
2024-06-07 16:39:40 +0900

    6월 7일

--- ExJava/App.java
+++
@@ -1,8 +0,0 @@
-import java2.Cal;
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        Cal ahn = new Cal();
-        System.out.println(ahn.add(12, 34));
-    }
-}
--- ExJava/java2/Cal.java
+++
@@ -1,7 +0,0 @@
-package java2;
-
-public class Cal {
-    public int add (int a, int b) {
-        return a+b;
-    }
-}
